7cae34d8688895b6b1db691e44ee380274a94872
Mihir Rabade <mihirrabade@gmail.com>
2021-10-12 20:53:30 +0530

    Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Discord-Ban-Utils-Bot
 Discord Ban Utilities for Mutual Servers!
+
+Invite Link - [click here](https://discord.com/api/oauth2/authorize?client_id=897454611370213436&permissions=277562263718&scope=bot%20applications.commands)
+
+## Bot Permissions
+
+![Bot Permissions](https://i.imgur.com/vth293y.png)
---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	}
+	else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+client.login(token);
