5811d5ee42abce95c863bad84c00ecbd8b54ae27
Mihir Rabade <mihirrabade@gmail.com>
2021-10-13 20:52:57 +0530

    Some changes to bypass 2k character limit

--- commands/export-ban-list.js
+++ commands/export-ban-list.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, time } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { token, pasteUser, pastePass, pasteKey } = require('../config.json');
@@ -17,29 +17,54 @@ module.exports = {
 		.setDescription('Exports ban list of current server'),
 
 	async execute(interaction) {
-		const timeString = time(date);
-		const outputFile = `../temp/${interaction.guild.name}-${timeString}.txt`;
+		// const timeString = time(date);
+
+		// `../temp/${interaction.guild.name}-${timeString}.txt`;
+		const ichannel = interaction.channel;
+		const iguildId = interaction.guildId;
+		const serverName = interaction.guild.name;
 
 		const bans = await rest.get(
-			Routes.guildBans(interaction.guildId),
+			Routes.guildBans(iguildId),
 		);
 
 		await interaction.reply(`Found ${bans.length} bans. Exporting...`);
+		const trips = Math.ceil(bans.length / 99);
 
-		let results = [];
-		// const reasons = [];
+		for (let trip = 0; trip < trips; trip++) {
 
-		bans.forEach((v) => {
-			results.push(v.user.id);
-			// , v.reason
-		});
+			let results = [];
+			// const reasons = [];
+			let i = 0 + trip * 99;
+			const len = 99 + trip * 99;
+			for (; i < len;i++) {
+				results.push(bans[i].user.id);
+				// , v.reason
+			}
 
-		results = JSON.stringify(results);
-		//	reasons = JSON.stringify(reasons);
 
-		//	fs.writeFile(outputFile, results);
-		await interaction.followUp(results);
-	//	await interaction.channel.send({ files:[outputFile] });
-	//	fs.unlink(outputFile);
+			results = JSON.stringify(results);
+			//	reasons = JSON.stringify(reasons);
+			const outputFile = `${serverName}-${date}.txt`;
+			paste.create({
+				contents: results,
+				name: outputFile,
+				expires: '1D',
+				anonymous: 'true',
+			},
+			function(success, data) {
+				if (success) {
+					return ichannel.send(`${results} \n\n ${data}`);
+				}
+				else {
+					return ichannel.send('There was some unexpected error.');
+				}
+			});
+
+			//	fs.writeFile(outputFile, results);
+
+		//	await interaction.channel.send({ files:[outputFile] });
+		//	fs.unlink(outputFile);
+		}
 	},
 };
