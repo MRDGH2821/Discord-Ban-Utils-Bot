7a6b2bc58399f404f764638d8d9f92aacf02b3cc
Mihir Rabade <mihirrabade@gmail.com>
2021-10-18 18:48:01 +0530

    Fixed Export bans command

--- commands/export-ban-list.js
+++ commands/export-ban-list.js
@@ -4,7 +4,6 @@ const { Routes } = require('discord-api-types/v9');
 const { token, pasteUser, pastePass, pasteKey } = require('../config.json');
 
 const paste = require('better-pastebin');
-// const fs = require('fs');
 
 const rest = new REST({ version: '9' }).setToken(token);
 const date = new Date();
@@ -17,54 +16,74 @@ module.exports = {
 		.setDescription('Exports ban list of current server'),
 
 	async execute(interaction) {
-		// const timeString = time(date);
-
-		// `../temp/${interaction.guild.name}-${timeString}.txt`;
-		const ichannel = interaction.channel;
-		const iguildId = interaction.guildId;
-		const serverName = interaction.guild.name;
-
 		const bans = await rest.get(
-			Routes.guildBans(iguildId),
+			Routes.guildBans(interaction.guildId),
 		);
+		await interaction.deferReply(`Found ${bans.length} bans. Exporting...`);
+		console.log(`Found ${bans.length} bans. Exporting...`);
 
-		await interaction.reply(`Found ${bans.length} bans. Exporting...`);
-		const trips = Math.ceil(bans.length / 99);
+		let results = [];
+		bans.forEach((v) => {
+			results.push(v.user.id);
+		});
+		results = JSON.stringify(results);
+		console.log(results);
 
-		for (let trip = 0; trip < trips; trip++) {
-
-			let results = [];
-			// const reasons = [];
-			let i = 0 + trip * 99;
-			const len = 99 + trip * 99;
-			for (; i < len;i++) {
-				results.push(bans[i].user.id);
-				// , v.reason
+		const outputFile = `${interaction.guild.name}-${date}.txt`;
+		paste.create({
+			contents: results,
+			name: outputFile,
+			expires: '1D',
+			anonymous: 'true',
+		},
+		function(success, data) {
+			if (success) {
+				console.log('PASTE SUCECESS');
+				console.log(data);
+				return interaction.editReply(data);
 			}
-
-
-			results = JSON.stringify(results);
-			//	reasons = JSON.stringify(reasons);
-			const outputFile = `${serverName}-${date}.txt`;
-			paste.create({
-				contents: results,
-				name: outputFile,
-				expires: '1D',
-				anonymous: 'true',
-			},
-			function(success, data) {
-				if (success) {
-					return ichannel.send(`${results} \n\n ${data}`);
-				}
-				else {
-					return ichannel.send('There was some unexpected error.');
-				}
-			});
-
-			//	fs.writeFile(outputFile, results);
-
-		//	await interaction.channel.send({ files:[outputFile] });
-		//	fs.unlink(outputFile);
-		}
+			else {
+				return interaction.editReply('There was some unexpected error.');
+			}
+		});
 	},
 };
+/*
+const fe = new_paste(interaction.serverName, results);
+console.log(fe);
+return interaction.editReply(fe);
+*/
+/*
+for (let trip = 0; trip < trips; trip++) {
+
+	let results = [];
+	// const reasons = [];
+	let i = 0 + trip * 50;
+	const len = 50 + trip * 50;
+	for (; i < len;i++) {
+		results.push(bans[i].user.id);
+		// , v.reason
+	}
+
+
+	results = JSON.stringify(results);
+	//	reasons = JSON.stringify(reasons);
+	const outputFile = `${serverName}-${date}.txt`;
+	paste.create({
+		contents: results,
+		name: outputFile,
+		expires: '1D',
+		anonymous: 'true',
+	},
+	function(success, data) {
+		if (success) {
+			return interaction.editReply(`${results} \n\n ${data}`);
+		}
+		else {
+			return interaction.editReply('There was some unexpected error.');
+		}
+	});
+*/
+//	fs.writeFile(outputFile, results);
+//	await interaction.channel.send({ files:[outputFile] });
+//	fs.unlink(outputFile);
