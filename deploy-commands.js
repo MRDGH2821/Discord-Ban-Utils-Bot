bed62d80d82be1f5c30977d611b39c5463f58fa0
Mihir Rabade <mihirrabade@gmail.com>
2021-10-19 16:32:02 +0530

    Added a comment for deleting commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -31,3 +31,24 @@ const rest = new REST({ version: '9' }).setToken(token);
 		console.error(error);
 	}
 })();
+
+/* All commands deleter
+		rest.get(Routes.applicationGuildCommands(clientId, guildId))
+			.then(data => {
+				const promises = [];
+				for (const command of data) {
+					const deleteUrl = `${Routes.applicationGuildCommands(clientId, guildId)}/${command.id}`;
+					promises.push(rest.delete(deleteUrl));
+				}
+				return Promise.all(promises);
+			});
+		rest.get(Routes.applicationCommands(clientId, guildId))
+			.then(data => {
+				const promises = [];
+				for (const command of data) {
+					const deleteUrl = `${Routes.applicationCommands(clientId, guildId)}/${command.id}`;
+					promises.push(rest.delete(deleteUrl));
+				}
+				return Promise.all(promises);
+			});
+			*/
