857519e2c74eadef6cae5fe0019bbdde1ecda4f5
Mihir Rabade <mihirrabade@gmail.com>
2021-10-20 11:42:39 +0530

    Import command now requires Ban permissions to work

--- commands/import-ban-list.js
+++ commands/import-ban-list.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+const { Permissions } = require('discord.js');
 const { token, pasteUser, pastePass, pasteKey } = require('../config.json');
 
 const paste = require('better-pastebin');
@@ -21,23 +22,28 @@ module.exports = {
 				.setRequired(true)),
 
 	async execute(interaction) {
-		const paste_id = interaction.options.getString('pastebin_link');
-		await interaction.reply('Parsing... (If it is taking long time, it means the link was invalid & bot crashed)');
-		paste.get(paste_id, function(success, data) {
-			if (success) {
-				const bans = JSON.parse(data);
-				bans.forEach((v) => {
-					console.log(`Banning user ID ${v}...`);
-					rest.put(
-						Routes.guildBan(interaction.guildId, v),
-						{ reason: `Ban Import on ${date.toDateString()}` },
-					);
-				});
-				return interaction.editReply(`${bans.length} bans are being imported in background. Sit back and relax for a while!`);
-			}
-			else {
-				return interaction.editReply('Given PasteBin link does not have contents in proper format...');
-			}
-		});
+		if (interaction.member.permissions.has([Permissions.FLAGS.BAN_MEMBERS]))	{
+			const paste_id = interaction.options.getString('pastebin_link');
+			await interaction.reply('Parsing... (If it is taking long time, it means the link was invalid & bot crashed)');
+			paste.get(paste_id, function(success, data) {
+				if (success) {
+					const bans = JSON.parse(data);
+					bans.forEach((v) => {
+						console.log(`Banning user ID ${v}...`);
+						rest.put(
+							Routes.guildBan(interaction.guildId, v),
+							{ reason: `Ban Import on ${date.toDateString()}` },
+						);
+					});
+					return interaction.editReply(`${bans.length} bans are being imported in background. Sit back and relax for a while!`);
+				}
+				else {
+					return interaction.editReply('Given PasteBin link does not have contents in proper format...');
+				}
+			});
+		}
+		else {
+			return interaction.reply('You cannot just ban anybody by importing 🤷. Contact Server Moderators!');
+		}
 	},
 };
