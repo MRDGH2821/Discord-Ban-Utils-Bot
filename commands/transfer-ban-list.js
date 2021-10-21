56b2ba21d7098f4f7b21d3e8fdabeebdd9964604
Mihir Rabade <mihirrabade@gmail.com>
2021-10-21 16:00:12 +0530

    Code cleanup Still not working

--- commands/transfer-ban-list.js
+++ commands/transfer-ban-list.js
@@ -5,8 +5,6 @@ const rest = new REST({ version: '9' }).setToken(token);
 
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageSelectMenu, MessageEmbed } = require('discord.js');
-let destname;
-let destid;
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -14,13 +12,14 @@ module.exports = {
 		.setDescription('Transfers Bans across servers'),
 
 	async execute(interaction) {
+		let destname;
+		let destid;
 		const initial_Screen = new MessageEmbed()
 			.setTitle('Ban List transferer')
 			.setDescription('Fetching Mutual Servers on which you can transfer bans to. \nPlease wait...');
-		//
 
 		const message = await interaction.reply({ embeds: [initial_Screen], fetchReply: true });
-		// = await interaction.fetchReply();
+
 		const collector = message.createMessageComponentCollector({ componentType: 'SELECT_MENU', time: 15000 });
 
 		const guilds = [];
@@ -34,14 +33,7 @@ module.exports = {
 				servers.push({ label: Object.entries(guilds)[i][1].name, value:Object.entries(guilds)[i][1].id });
 			}
 		}
-		/*
-		const currentServer = servers.findIndex(element => {
-			if (element.label === interaction.guild.name) {
-				return true;
-			}
-		});
-		delete servers[currentServer];
-		*/
+
 		console.log(servers);
 		const row = new MessageActionRow()
 			.addComponents(
@@ -53,25 +45,26 @@ module.exports = {
 			);
 
 		initial_Screen.setDescription('Select Target Server where you wish to transfer bans. Bans will be transferred from current server');
+
 		await interaction.editReply({ embeds:[initial_Screen], components: [row], fetchReply: true });
-		//	console.log(`Interaction has: \n${interaction}`);
-		console.log('\n\n\n');
 
-
-		const serverID = collector.on('collect', i => {
+		collector.on('collect', i => {
 			if (i.user.id === interaction.user.id) {
+
 				destname = interaction.client.guilds.cache.get(i.values[0]).name;
 				destid = interaction.client.guilds.cache.get(i.values[0]).id;
+
 				console.log(destname);
 				console.log(destid);
-				initial_Screen.setDescription(`Source server: ${interaction.guild.name}\nDestination Server: ${interaction.client.guilds.cache.get(i.values[0]).name}`);
-				interaction.editReply({ embeds:[initial_Screen], components: [], fetchReply: true });
 
+				initial_Screen
+					.setDescription(`Source server: ${interaction.guild.name}\nDestination Server: ${interaction.client.guilds.cache.get(i.values[0]).name}`);
+
+				interaction.editReply({ embeds:[initial_Screen], components: [], fetchReply: true });
 			}
 			else {
 				i.reply({ content: 'These buttons aren\'t for you!', ephemeral: true });
 			}
-			return i.values[0];
 		});
 
 		collector.on('end', collected => {
@@ -83,72 +76,23 @@ module.exports = {
 			console.log(`Collected ${collected.size} interactions. Collected: ${collected}`);
 		});
 
+		const fromGuildId = interaction.guild.id;
+		const toGuildId = destid;
+		console.log(`Fetching bans for guild ${fromGuildId}...`);
+
 		const bans = await rest.get(
-			Routes.guildBans(interaction.guild.id),
+			Routes.guildBans(fromGuildId),
 		);
-		console.log(`Applying bans to guild ${interaction.client.guilds.cache.get(serverID).name}...`);
+		console.log(`Found ${bans.length} bans.`);
+		console.log(`Applying bans to guild ${toGuildId}...`);
 		for (const v of bans) {
 			console.log(`Banning user ${v.user.username}#${v.user.discriminator}...`);
 			await rest.put(
-				Routes.guildBan(interaction.client.guilds.cache.get(serverID).id, v.user.id),
+				Routes.guildBan(toGuildId, v.user.id),
 				{ reason: v.reason },
 			);
 		}
+		console.log(`Successfully transferred all bans from ${fromGuildId} to ${toGuildId}.`);
+
 	},
 };
-/*
-		const filter = i =>	interaction.isSelectMenu() && i.user.id === interaction.user.id;
-		const collector = SelectMenuInteraction.message.createMessageComponentCollector({
-			filter,
-			max: 1,
-		});
-
-		collector.on('collect', async (collected) => {
-			const value = collected.values[0];
-			console.log(value);
-			collected.deferUpdate();
-			return interaction.followUp(value);
-		});
-*/
-
-
-/*
-		const collector = message.createMessageComponentCollector({ componentType: 'BUTTON', time: 15000 });
-
-		collector.on('collect', i => {
-			if (i.user.id === interaction.user.id) {
-				i.editReply(`${i.user.id} clicked on the ${i.customId} button.`);
-			}
-			else {
-				i.editReply({ content: 'These buttons aren\'t for you!', ephemeral: true });
-			}
-		});
-
-		collector.on('end', collected => {
-			console.log(`Collected ${collected.size} interactions.`);
-		});
-*/
-/*
-const filter = i => {
-	i.deferUpdate();
-	return i.user.id === interaction.user.id;
-};
-		interaction.SelectMenuInteraction.message.awaitMessageComponent({ filter, componentType: 'SELECT_MENU', time: 60000 })
-			.then(console.log(`You selected ${interaction.values.join(', ')}!`))
-			.catch(err => console.log(`No interactions were collected. ${err}`));
-	},
-	*/
-/*
-console.log(Object.keys(guilds).length);
-console.log('\n\n\n');
-// Specific server's info
-console.log(Object.entries(guilds)[0][1]);
-console.log('\n\n\n');
-console.log(typeof Object.entries(guilds)[0][1]);
-console.log('\n\n\n');
-// Server's name & ID
-console.log(Object.entries(guilds)[0][1].id, Object.entries(guilds)[0][1].name);
-console.log('\n\n\n');
-// 2nd Server's name & ID
-console.log(Object.entries(guilds)[1][1].id, Object.entries(guilds)[1][1].name);
-*/
