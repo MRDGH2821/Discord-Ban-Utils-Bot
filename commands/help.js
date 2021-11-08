a8705064f02ce1fd575a32181fc361c057cfec7c
Mihir Rabade <mihirrabade@gmail.com>
2021-11-08 17:16:45 +0530

    Command names updated

--- commands/help.js
+++ commands/help.js
@@ -9,14 +9,14 @@ const helpEmbed = new MessageEmbed()
 	.setURL('https://discord.gg/MPtE9zsBs5')
 	.setDescription('The help section you get you started!\nThe bot helps you import, export & transfer bans from one server to another.')
 	.addFields(
-		{ name: 'Ban', value: 'Bans the given user.' },
-		{ name: 'Unban', value: 'Unbans the given user.' },
-		{ name: 'Import', value: 'Imports the ban list from pastebin link' },
-		{ name: 'Export', value: 'Exports the Ban list to pastebin' },
-		{ name: 'Transfer', value: 'Exports the ban list from current server & imports it into target server without extra steps.' },
-		{ name: 'Kick', value: 'Kicks the given user.' },
-		{ name: 'Ping', value: 'Bot Latency.' },
-		{ name: 'User-info', value: 'Tells if you have the permissions to kick or ban .' },
+		{ name: '/Ban', value: 'Bans the given user.' },
+		{ name: '/Unban', value: 'Unbans the given user.' },
+		{ name: '/Import', value: 'Imports the ban list from pastebin link' },
+		{ name: '/Export', value: 'Exports the Ban list to pastebin' },
+		{ name: '/Transfer', value: 'Exports the ban list from current server & imports it into target server without extra steps.' },
+		{ name: '/Kick', value: 'Kicks the given user.' },
+		{ name: '/Ping', value: 'Bot Latency.' },
+		{ name: '/User-info', value: 'Tells if you have the permissions to kick or ban .' },
 		{ name: 'Bot still not working?', value: 'Please join my server & elaborate how you encountered that problem. You may also submit an issue at [Github Repository](https://github.com/MRDGH2821/Discord-Ban-Utils-Bot/issues)' },
 	)
 	.setFooter(`Bot version ${version}`);
