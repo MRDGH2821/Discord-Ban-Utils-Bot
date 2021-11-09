9d6f5ad5a514d95dc23fc5d3acb52ee15b61f634
Mihir Rabade <mihirrabade@gmail.com>
2021-11-09 12:04:28 +0530

    Added Invite Rows

--- commands/ban.js
+++ commands/ban.js
@@ -3,6 +3,7 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { token } = require('../betaconfig.json');
 const { Permissions } = require('discord.js');
+const { InviteRow, SupportRow } = require('../lib/RowButtons.js');
 const rest = new REST({ version: '9' }).setToken(token);
 const date = new Date().toDateString();
 
@@ -27,9 +28,11 @@ module.exports = {
 		const reas = interaction.options.getString('reason');
 		try {
 			if (interaction.guild) {
-				if (interaction.member.permissions.has([Permissions.FLAGS.BAN_MEMBERS])) {
+				if (
+					interaction.member.permissions.has([Permissions.FLAGS.BAN_MEMBERS])
+				) {
 					if (reas === null) {
-					// If no reason given, give a formatted reason
+						// If no reason given, give a formatted reason
 						await rest.put(Routes.guildBan(interaction.guildId, target.id), {
 							reason: `Banned by ${interaction.user.tag} on ${date} for "no reason"`,
 						});
@@ -39,7 +42,7 @@ module.exports = {
 						});
 					}
 					else {
-					// When a reason is given.
+						// When a reason is given.
 						await rest.put(Routes.guildBan(interaction.guildId, target.id), {
 							reason: reas,
 						});
@@ -49,17 +52,25 @@ module.exports = {
 					}
 				}
 				else {
-					await interaction.reply('You cannot ban...');
+					await interaction.reply({
+						content: 'You cannot ban...',
+						components: [InviteRow],
+					});
 				}
 			}
 			else {
-				await interaction.reply(
-					'Are you sure you are in a server to execute this?:unamused:  \nBecause this command can only be used in Server Text channels or Threads :shrug:',
-				);
+				await interaction.reply({
+					content:
+						'Are you sure you are in a server to execute this?:unamused:  \nBecause this command can only be used in Server Text channels or Threads :shrug:',
+					components: [InviteRow],
+				});
 			}
 		}
 		catch (e) {
-			interaction.reply(`There was some error while executing this command:\n\`${e}\``);
+			interaction.reply({
+				content: `There was some error while executing this command:\n\`${e}\``,
+				components: [SupportRow],
+			});
 		}
 	},
 };
