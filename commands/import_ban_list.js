745c4d0860785771b71cf33497ee399222ee1468
Mihir Rabade <mihirrabade@gmail.com>
2021-11-20 17:53:50 +0530

    Migrated to dpaste implementation

--- commands/import_ban_list.js
+++ commands/import_ban_list.js
@@ -22,52 +22,47 @@ module.exports = {
 		),
 
 	async execute(interaction) {
-		const paste_id = interaction.options.getString('pastebin_link');
+		const paste_id = PasteCheck(interaction.options.getString('pastebin_link'));
 		if (interaction.guild) {
 			// User should have ban permissions else it will not work
 			if (interaction.member.permissions.has([Permissions.FLAGS.BAN_MEMBERS])) {
 				await interaction.reply(
 					'Parsing... (If it is taking long time, it means the link was invalid & bot has probably crashed)',
 				);
-				dpst
-					.GetRawPaste(paste_id)
-					.then(async data => {
-						// Try to parse the data.
-						// If it doesn't work, input link was invalid.
-
-						const rawEle = data.split(/[^\d{18}]+/g);
-						const bans = rawEle.map(element => element.trim());
-
-						console.log(typeof bans);
-						console.log(bans);
-
-						// console.log(JSON.parse(data));
-						await interaction.editReply(
-							`${bans.length} bans are being imported in background. Sit back and relax for a while!`,
-						);
-						let validBans = bans.length;
-						// Ban users
-						bans.forEach(async v => {
-							console.log(`Banning user ID ${v}...`);
-							await rest
-								.put(Routes.guildBan(interaction.guildId, v), {
-									reason: `Ban Import on ${date.toDateString()}`,
-								})
-								.catch(() => {
-									validBans = validBans - 1;
-								});
-						});
-						await interaction.editReply(
-							`Ban List: ${bans.length}. \nInvalid Bans: ${bans.length -
-								validBans}.\n${validBans} imported successfully!`,
-						);
-					})
-					.catch(async e => {
-						// When the link is invalid. this code prevented earlier versions of crashes.
-						await interaction.editReply(
-							`Given PasteBin link is invalid...\nLink: ${paste_id} \nError dump:\n\`${e}\``,
-						);
+				const data = await dpst.GetRawPaste(paste_id);
+				try {
+					const rawEle = data.split(/\D+/g);
+					const bans = rawEle.map(element => element.trim());
+					await interaction.client.users.fetch(bans[0]);
+					await interaction.editReply(
+						`${bans.length} bans are being imported in background. Sit back and relax for a while!`,
+					);
+					let validBans = bans.length;
+					// Ban users
+					bans.forEach(async v => {
+						try {
+							console.log(
+								`Banning user ID ${await interaction.client.users.fetch(v)}...`,
+							);
+							await rest.put(Routes.guildBan(interaction.guildId, v), {
+								reason: `Ban Import on ${date.toDateString()}`,
+							});
+						}
+						catch {
+							validBans = validBans - 1;
+						}
 					});
+					await interaction.editReply(
+						`Ban List: ${bans.length}. \nInvalid Bans: ${bans.length -
+							validBans}.\n${validBans} imported successfully!`,
+					);
+				}
+				catch (e) {
+					// When the link is invalid. this code prevented earlier versions of crashes.
+					await interaction.editReply(
+						`Given PasteBin link is invalid...\nLink: https://dpaste.com/${paste_id} \nError dump:\n\`${e}\``,
+					);
+				}
 			}
 			else {
 				// When people do not have the permissions to ban.
