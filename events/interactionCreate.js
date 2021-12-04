135ba07e281d7851892512e4784b64b418115ca8
MRDGH2821 <mihirrabade@gmail.com>
2021-12-04 18:51:55 +0530

    Added Interaction activities

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -4,5 +4,24 @@ module.exports = {
 		console.log(
 			`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`,
 		);
+
+		const banHammer = [
+			'ban',
+			'export_ban_list',
+			'import_ban_list',
+			'mass_ban',
+			'transfer_bans',
+		];
+		const info = ['user_info', 'help'];
+		if (banHammer.includes(interaction.commandName)) {
+			interaction.client.user.setActivity('a ban hammer being dropped', {
+				type: 'WATCHING',
+			});
+		}
+		else if (info.includes(interaction.commandName)) {
+			interaction.client.user.setActivity('to tech support requests', {
+				type: 'LISTENING',
+			});
+		}
 	},
 };
