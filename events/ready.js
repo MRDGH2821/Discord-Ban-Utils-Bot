6d008bb784b62370fcd042dcf4e873464979ba95
MRDGH2821 <mihirrabade@gmail.com>
2021-12-04 19:27:15 +0530

    Added three activities

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,32 @@
+const { version } = require('../version.json');
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 module.exports = {
 	name: 'ready',
 	once: true,
-	execute(client) {
+	async execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		const activities = [
+			{
+				text: `/help in ${client.guilds.cache.size} servers`,
+				obj: { type: 'LISTENING' },
+			},
+			{
+				text: 'a ban hammer being dropped',
+				obj: { type: 'WATCHING' },
+			},
+			{
+				text: `bot version ${version}`,
+				obj: { type: 'PLAYING' },
+			},
+		];
+		while (activities) {
+			await sleep(5);
+			const act = activities[Math.floor(Math.random() * activities.length)];
+			client.user.setActivity(act.text, act.obj);
+		}
 	},
 };
