2aa08a5f21fd9b297768730eacfef894b9c9e954
MRDGH2821 <mihirrabade@gmail.com>
2022-01-04 19:14:27 +0530

    Fix if-else hell and other things Fixed Description. Added Default reason New way of checking permissions.

--- commands/timeout.js
+++ commands/timeout.js
@@ -12,16 +12,21 @@ module.exports = {
         .setDescription('Tag a user')
         .setRequired(true),
     )
-    .addStringOption((option) =>
-      option.setName('reason').setDescription('Enter reason for Timeout'),
-    )
     .addIntegerOption((option) =>
       option
         .setName('duration')
         .setDescription(
           'Enter Duration in minutes. Put 0 to remove timeout. Max 4 weeks.',
         )
-        .setMaxValue(4 * 24 * 60 * 1000),
+        .setMaxValue(5760000)
+        .setRequired(true),
+    )
+    .addStringOption((option) =>
+      option
+        .setName('reason')
+        .setDescription(
+          'Enter reason for Timeout. Default: Timed-out by <you> for <duration> on <today\'s date>',
+        ),
     )
     .addBooleanOption((option) =>
       option.setName('dm_reason').setDescription('Send Reason as DM?'),
@@ -29,60 +34,48 @@ module.exports = {
 
   async execute(interaction) {
     const target = interaction.options.getMember('user');
-    const reason = interaction.options.getString('reason');
     const duration = interaction.options.getInteger('duration');
-    const dm_reason = interaction.options.getBoolean('dm_reason');
+    const reason =
+      interaction.options.getString('reason') ||
+      `Timed-out by ${
+        interaction.user.tag
+      } for ${duration} mins on ${new Date().toString()}`;
+    const dm_reason = interaction.options.getBoolean('dm_reason') || false;
     try {
-      if (interaction.guild) {
-        if (
-          interaction.member.permissions.has([
-            Permissions.FLAGS.MODERATE_MEMBERS,
-          ])
-        ) {
-          // Checks if target user can be managed or not
-          if (target.managable) {
-            // If there is a reason specified, DM it to the user.
-            if (dm_reason) {
-              try {
-                await target.user.send(
-                  `Reason for timeout from ${interaction.guild.name}: ${reason}`,
-                );
-              }
-              catch (e) {
-                console.log('Reason cannot be DM-ed');
-              }
-            }
-            await interaction.reply({
-              content: `User \`${target.user.tag}\` is timed-out from this server.`,
-            });
-            await target.timeout(duration * 60 * 1000, reason);
-          }
-          // If user cannot be timed-out
-          else {
-            await interaction.reply({
-              content: `User \`${target.user.tag}\` cannot be timed-out :grimacing:.`,
-            });
-          }
-        }
-        // If you don't have permissions to kick
-        else {
-          await interaction.reply({
-            content: 'You cannot timeout anyone...',
-            components: [InviteRow],
-          });
-        }
+      if (!interaction.guild) {
+        throw 'Are you sure you are in a server to execute this?:unamused: \nBecause this command can only be used in Server Text channels or Threads :shrug:';
+      }
+      if (
+        !interaction.member.permissions.has([
+          Permissions.FLAGS.MODERATE_MEMBERS,
+        ])
+      ) {
+        await interaction.reply({
+          content: 'You cannot Timeout members.',
+          components: [InviteRow],
+        });
       }
       else {
+        await target.timeout(duration * 60 * 1000, reason);
+        if (dm_reason) {
+          // If there is a reason specified, DM it to the user.
+          target.user
+            .send(
+              `You have been timed-out from ${interaction.guild.name}.\nReason: ${reason}\nDuration: ${duration}`,
+            )
+            .catch('User cannot be DM-ed');
+        }
+
         await interaction.reply({
-          content:
-            'Are you sure you are in a server to execute this?:unamused: \nBecause this command can only be used in Server Text channels or Threads :shrug:',
-          components: [InviteRow],
+          content: `User ${target.user.tag} is timed-out.\nReason: ${reason}\nDuration: ${duration} minutes`,
         });
       }
     }
     catch (e) {
+      // If any error is thrown
+
       await interaction.reply({
-        content: `Unexpected Error Occured! \nPlease Report to the Developer. \nError Dump:\n\`${e}\``,
+        content: `Error Occured! \nPlease Report to the Developer. \nError Dump:\n${e}`,
         components: [SupportRow],
       });
     }
