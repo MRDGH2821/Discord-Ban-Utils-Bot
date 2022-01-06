8e5e2b01254ba739e4a13575bbf441601533e50b
MRDGH2821 <mihirrabade@gmail.com>
2022-01-06 23:56:25 +0530

    New logic for webhook checking Will now reply in embeds

--- commands/set_logs.js
+++ commands/set_logs.js
@@ -65,11 +65,21 @@ module.exports = {
           webhook = await newHook(channel);
         }
         else {
-          webhook = await changeHook(
-            interaction.client,
-            channel,
-            serverDB.data().logChannelID,
+          const oldChannel = await interaction.client.channels.cache.get(
+            serverData.logChannelID,
           );
+          try {
+            const webhooks = await oldChannel.fetchWebhooks();
+            const validHook = webhooks.find((wh) => wh.token);
+            webhook = await changeHook(
+              interaction.client,
+              channel,
+              validHook.id,
+            );
+          }
+          catch (e) {
+            webhook = await newHook(channel);
+          }
         }
         const data = {
           logChannelID: channelID,
@@ -82,15 +92,29 @@ module.exports = {
           .doc(`${serverID}`)
           .set(data, { merge: true });
 
-        await interaction.editReply(
-          `Configured <#${channel.id}> as logging channel.`,
-        );
+        await interaction.editReply({
+          //  content: `Configured <#${channel.id}> as logging channel.`,
+          embeds: [
+            {
+              color: 0xd8d4d3,
+              title: 'Log Channel configured!',
+              description: ` Configured <#${channel.id}> as logging channel.`,
+            },
+          ],
+        });
       }
       else {
         const logChannel = serverData.logChannelID;
         console.log('LogChannel: ', logChannel);
         await interaction.editReply({
-          content: `Log channel is already configured to <#${logChannel}>.\nRerun this command with \`force_update\` set to \`True\` if there is some problem.`,
+          // content: `Log channel is already configured to <#${logChannel}>.\nRerun this command with \`force_update\` set to \`True\` if there is some problem.`,
+          embeds: [
+            {
+              color: 0xd8d4d3,
+              title: 'Log Channel already configured!',
+              description: `Log channel is already configured to <#${logChannel}>.\nRerun this command with \`force_update\` set to \`True\` if there is some problem.`,
+            },
+          ],
         });
       }
     }
