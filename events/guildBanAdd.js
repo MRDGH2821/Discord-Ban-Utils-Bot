74bbb350c7561881610d29b713eb362649c2790e
MRDGH2821 <mihirrabade@gmail.com>
2022-02-02 22:13:39 +0530

    fix: add logic to prevent duplicate ban log

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -2,56 +2,58 @@ const { db } = require('../lib/firebase.js');
 
 module.exports = {
   name: 'guildBanAdd',
+  // eslint-disable-next-line sort-keys
   async execute(ban) {
-    //	const bannedUser = await ban.user.fetch();
-    //	console.log(bannedUser);
-    //	console.log('A member is banned');
-    //	console.log(ban);
     const embed = {
-      color: 0xe1870a,
-      title: '**User banned!**',
-      thumbnail: { url: ban.user.displayAvatarURL({ dynamic: true }) },
-      fields: [
-        {
-          name: '**Reason**',
-          value: `${ban.reason}`,
-        },
-      ],
-      footer: { text: `ID: ${ban.user.id}` },
-      timestamp: new Date(),
-    };
+        color: 0xe1870a,
+        title: '**User banned!**',
+        // eslint-disable-next-line sort-keys
+        thumbnail: { url: ban.user.displayAvatarURL({ dynamic: true }) },
+        // eslint-disable-next-line sort-keys
+        fields: [
+          {
+            name: '**Reason**',
+            value: `${ban.reason}`
+          }
+        ],
+        footer: { text: `ID: ${ban.user.id}` },
+        timestamp: new Date()
+      },
+      fetchedLogs = await ban.guild.fetchAuditLogs({
+        limit: 1,
+        type: 'MEMBER_BAN_ADD'
+      }),
+      // Since there's only 1 audit log entry in this collection, grab the first one
+      firstBanLog = fetchedLogs.entries.first(),
 
-    const fetchedLogs = await ban.guild.fetchAuditLogs({
-      limit: 1,
-      type: 'MEMBER_BAN_ADD',
-    });
-    // Since there's only 1 audit log entry in this collection, grab the first one
-    const banLog = fetchedLogs.entries.first();
+      /*
+       * Now grab the user object of the person who banned the member
+       * Also grab the target of this action to double-check things
+       */
+      { executor, target } = firstBanLog;
 
     // Perform a coherence check to make sure that there's *something*
-    if (!banLog) {
+    if (!firstBanLog) {
       const line = `${ban.user.tag} ${ban.user} was banned from ${ban.guild.name}.`;
       embed.description = line;
       embed.fields.push({
         name: '**Justice Hammer Wielder**',
-        value: 'No audit log could be found.',
+        value: 'No audit log could be found.'
       });
       console.log(line, 'No audit log could be found.');
     }
 
-    // Now grab the user object of the person who banned the member
-    // Also grab the target of this action to double-check things
-    const { executor, target } = banLog;
-
-    // Update the output with a bit more information
-    // Also run a check to make sure that the log returned was for the same banned member
+    /*
+     * Update the output with a bit more information
+     * Also run a check to make sure that the log returned was for the same banned member
+     */
     if (target.id === ban.user.id) {
       const line = `${ban.user.tag} ${ban.user} got hit with the swift hammer of justice in the guild ${ban.guild.name}!`;
       console.log(line, 'Justice Hammer Wielder: ', executor.tag);
       embed.description = line;
       embed.fields.push({
         name: '**Justice Hammer Wielder**',
-        value: `${executor.tag} ${executor}`,
+        value: `${executor.tag} ${executor}`
       });
     }
     else {
@@ -60,35 +62,39 @@ module.exports = {
       embed.description = line;
       embed.fields.push({
         name: '**Justice Hammer Wielder**',
-        value: 'Audit log fetch was inconclusive.',
+        value: 'Audit log fetch was inconclusive.'
       });
     }
 
-    const serverDB = await db
-      .collection('servers')
-      .doc(ban.guild.id)
+    // eslint-disable-next-line one-var
+    const serverDB = await db.collection('servers').doc(ban.guild.id)
       .get();
 
     try {
-      if (serverDB.exists) {
-        const serverData = serverDB.data();
-        console.log('Doc data: ', serverData);
-        console.log('logWebHookID: ', serverData.logWebhookID);
-        const webhookID = serverData.logWebhookID;
+      const isBannedViaCmd = ban.client.user.id === executor.id;
+      console.log('Ban.client: ', ban.client.user.id);
+      console.log('Executor: ', executor.id);
+      if (!isBannedViaCmd) {
+        if (serverDB.exists) {
+          const serverData = serverDB.data(),
+            webhookID = serverData.logWebhookID;
+          console.log('Doc data: ', serverData);
+          console.log('logWebHookID: ', serverData.logWebhookID);
 
-        if (webhookID) {
-          const webhookClient = await ban.guild.client.fetchWebhook(webhookID);
-          webhookClient.send({
-            embeds: [embed],
-          });
+          if (webhookID) {
+            const webhookClient = await ban.guild.client.fetchWebhook(webhookID);
+            webhookClient.send({
+              embeds: [embed]
+            });
+          }
         }
-      }
-      else {
-        console.log(`No log channel configured for ${ban.guild.name}`);
+        else {
+          console.log(`No log channel configured for ${ban.guild.name}`);
+        }
       }
     }
     catch (error) {
       console.log(error);
     }
-  },
+  }
 };
