c9d4338138a913a4494e887a60fb4d22ddba52a4
MRDGH2821 <mihirrabade@gmail.com>
2022-02-05 17:52:26 +0530

    chore: update pm2 ecosystem file

--- ban-utils.config.js
+++ ban-utils.config.js
@@ -1,9 +1,21 @@
 module.exports = {
   apps: [
     {
+      env_dev: {
+        NODE_ENV: 'dev'
+      },
+      env_prod: {
+        NODE_ENV: 'prod'
+      },
       name: 'Ban Utils Bot',
       script: 'index.js',
-      watch: '.'
+      watch: true
+    },
+    {
+      autorestart: false,
+      name: 'Deploy BU Bot',
+      script: './deploy-check.js',
+      watch: false
     }
   ]
 };
