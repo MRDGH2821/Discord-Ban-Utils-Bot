163cba2d3d552633424196bc71a22405a50e0b36
MRDGH2821 <mihirrabade@gmail.com>
2022-02-08 21:22:48 +0530

    feat: not send reason as DM
    Kick command is also refactored

--- commands/kick.js
+++ commands/kick.js
@@ -1,98 +1,91 @@
-const { Permissions } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { InviteRow, SupportRow } = require('../lib/RowButtons.js');
-const { NotInsideServer, NoPerms } = require('../lib/ErrorEmbeds.js');
+const { MessageEmbed, Permissions } = require('discord.js');
+const { SupportRow, InviteRow } = require('../lib/RowButtons');
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('kick')
     .setDescription('Kicks a user')
     .addUserOption((option) => option.setName('user').setDescription('Tag a user')
       .setRequired(true))
-    .addStringOption((option) => option
-      .setName('reason')
-      .setDescription('Enter reason for Kick. Will be sent as DM to user')),
+    .addStringOption((option) => option.setName('reason').setDescription('Enter reason for Kick if any.')),
 
+  note: 'Default reason is: Kicked by <you> on <today\'s date>.',
+
+  // eslint-disable-next-line sort-keys
   async execute(interaction) {
+    await interaction.deferReply();
     const reason =
         (await interaction.options.getString('reason')) ||
-        '||for no reason :joy:||',
+        `Kicked by ${
+          interaction.user.tag
+        } on ${new Date().toDateString()} ||for no reason :joy:||`,
       target = await interaction.options.getMember('user');
-    //  const tartag = target.user.tag;
+
+    let canKick = false,
+      isInGuild = false,
+      isKickable = false;
+
     try {
-      if (!interaction.guild) {
-        await interaction.reply({
-          components: [InviteRow],
-          embeds: [NotInsideServer]
-        });
-      }
-      else if (
-        !interaction.member.permissions.has([Permissions.FLAGS.KICK_MEMBERS])
-      ) {
-        NoPerms.fields = [
-          {
-            name: '**Permissions Required**',
-            value: 'KICK_MEMBERS'
-          }
-        ];
-        await interaction.reply({
-          components: [InviteRow],
-          embeds: [NoPerms]
-        });
-      }
-      // checks if target user can be kicked or not
-      else if (target.kickable) {
-        // if there is a reason specified, DM it to the user.
-        if (reason) {
-          try {
-            await target.user.send(`Reason for kicking from ${interaction.guild.name}: ${reason}`);
-          }
-          catch (error) {
-            console.log('Reason cannot be DM-ed');
-          }
-        }
-        await interaction.reply({
-          // content: `User \`${target.tag}\` is banned from this server. \nReason: ${reas}.`,
-          embeds: [
+      canKick = await interaction.member.permissions.has([Permissions.FLAGS.KICK_MEMBERS]);
+      isKickable = target.kickable;
+      isInGuild = await interaction.guild;
+
+      if (isInGuild && canKick) {
+        await interaction.guild.members.kick(target, reason);
+
+        const kick_success = new MessageEmbed()
+          .setColor('84929f')
+          .setTitle('**Kicking Wrench Deployed!**')
+          .setDescription(`User \`${target.user.tag}\` ${target} is kicked from this server!`)
+          .setThumbnail(target.displayAvatarURL({ dynamic: true }))
+          .addFields([
             {
-              color: 0x84929f,
-              title: '**Kicking Wrench Deployed!**',
-              // eslint-disable-next-line sort-keys
-              description: `User \`${target.user.tag}\` ${target} is kicked from this server!`,
-              thumbnail: { url: target.displayAvatarURL({ dynamic: true }) },
-              // eslint-disable-next-line sort-keys
-              fields: [
-                {
-                  name: '**Reason**',
-                  value: `${reason}`
-                }
-              ]
+              name: '**Reason**',
+              value: reason
             }
-          ]
+          ]);
+
+        await interaction.editReply({
+          embeds: [kick_success]
         });
-        await target.kick();
       }
-      // if user cannot be kicked
       else {
-        await interaction.reply({
-          // content: 'Kicking Wrench cannot kick...',
-          components: [SupportRow],
-          embeds: [
-            {
-              title: '**Cannot Kick...**',
-              // eslint-disable-next-line sort-keys
-              description: `User ${target} cannot be kicked :grimacing:\n\nPlease move the bot role higher than that user for this command to work.`,
-              // eslint-disable-next-line sort-keys
-              color: 0xff0033
-            }
-          ]
-        });
+        throw new Error(`Inside server? ${isInGuild}\nCan Kick? ${canKick}\nTarget kickable? ${isKickable}`);
       }
     }
     catch (error) {
-      await interaction.reply({
-        components: [SupportRow],
-        content: `Unexpected Error Occured! \nPlease Report to the Developer. \nError Dump:\n\`${error}\``
+      const kick_fail = new MessageEmbed()
+        .setColor('ff0033')
+        .setTitle('**Cannot Kick...**')
+        .setDescription(`User ${target} cannot be kicked :grimacing:\n\nIf this error is comming even after passing all checks, then please report the Error Dump section to developer.`)
+        .addFields([
+          {
+            name: '**Checks**',
+            value: `Executed In server? **\`${isInGuild}\`**\nCan you kick? **\`${canKick}\`**\nTarget kickable? **\`${isKickable}\`**`
+          },
+          {
+            name: '**Possible solutions**',
+            value:
+              'Use this command inside a server where you have required permissions. Also make sure the bot role is above that user\'s highest role for this command to work.'
+          },
+          {
+            name: '**Inputs given**',
+            value: `User: ${target}  ${target.id}\nReason: ${reason}`
+          },
+          {
+            name: '**Bot Error Dump**',
+            value: `${error}`
+          }
+        ]);
+      await interaction.editReply({
+        components: [
+          SupportRow,
+          InviteRow
+        ],
+        embeds: [kick_fail]
       });
+      console.log(error);
     }
   }
 };
