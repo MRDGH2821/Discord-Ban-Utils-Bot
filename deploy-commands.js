775c44a1bebb99bb1cbc894c7dcc5c91f48e42bd
MRDGH2821 <mihirrabade@gmail.com>
2022-02-11 22:02:53 +0530

    chore: remove unused lines of code

--- deploy-commands.js
+++ deploy-commands.js
@@ -8,9 +8,7 @@ const { clientId, guildId, token } = require('./lib/ConfigManager.js'),
   commands = [].map((command) => command.toJSON()),
   rest = new REST({ version: '9' }).setToken(token);
 for (const file of commandFiles) {
-  const command = require(`./commands/${file}`),
-    data = command.data.toJSON();
-  data.note = command.note || 'none';
+  const command = require(`./commands/${file}`);
   commands.push(command.data.toJSON());
 }
 
