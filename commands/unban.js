a63fc9a5956c60ec44983ac5844eed311481bbde
MRDGH2821 <mihirrabade@gmail.com>
2022-02-12 16:17:32 +0530

    refactor: refactor unban command

--- commands/unban.js
+++ commands/unban.js
@@ -1,7 +1,6 @@
-const { Permissions } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { InviteRow, SupportRow } = require('../lib/RowButtons.js');
-const { NotInsideServer, NoPerms } = require('../lib/ErrorEmbeds.js');
+const { Permissions, MessageEmbed } = require('discord.js');
+const { SupportRow, InviteRow } = require('../lib/RowButtons');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -14,52 +13,63 @@ module.exports = {
 
   async execute(interaction) {
     await interaction.deferReply();
-    const target = await interaction.options.getUser('user');
+    const isInGuild = await interaction.inGuild(),
+      target = await interaction.options.getUser('user');
+    let canUnban = false;
     try {
-      if (!interaction.guild) {
-        await interaction.editReply({
-          components: [InviteRow],
-          embeds: [NotInsideServer]
-        });
-      }
-      else if (
-        interaction.member.permissions.has([Permissions.FLAGS.BAN_MEMBERS])
-      ) {
+      canUnban = await interaction.member.permissions.has([Permissions.FLAGS.BAN_MEMBERS]);
+      if (isInGuild && canUnban) {
         await interaction.guild.members.unban(target);
+
+        // eslint-disable-next-line one-var
+        const unban_success = new MessageEmbed()
+          .setTitle('**User Unbanned!**')
+          .setColor('e1870a')
+          .setDescription(`User \`${target.tag}\` ${target} is unbanned from this server.\nID: \`${target.id}\``)
+          .setThumbnail(target.displayAvatarURL({ dynamic: true }))
+          .setTimestamp();
+
         await interaction.editReply({
-          embeds: [
-            {
-              color: 0xe1870a,
-              title: '**User Unbanned!**',
-              // eslint-disable-next-line sort-keys
-              description: `User \`${target.tag}\` ${target} is unbanned from this server.`,
-              thumbnail: { url: target.displayAvatarURL({ dynamic: true }) },
-              timestamp: new Date(),
-              // eslint-disable-next-line sort-keys
-              footer: {
-                text: `${target.id}`
-              }
-            }
-          ]
+          embeds: [unban_success]
         });
       }
       else {
-        // when no ban permissions
-        NoPerms.fields = {
-          name: '**Permissions required**',
-          value: 'BAN_MEMBERS'
-        };
-        await interaction.editReply({
-          components: [InviteRow],
-          embeds: [NoPerms]
-        });
+        throw new Error(`Inside server? ${isInGuild}\nCan Unban? ${canUnban}`);
       }
     }
     catch (error) {
+      const unban_fail = new MessageEmbed()
+        .setColor('ff0033')
+        .setTitle('**Cannot Unban...**')
+        .setDescription(`User ${target} cannot be unbanned :grimacing:\n\nIf this error is comming even after passing all checks, then please report the Error Dump section to developer.`)
+        .addFields([
+          {
+            name: '**Checks**',
+            value: `Executed In server? **\`${isInGuild}\`**\nCan you unban? **\`${canUnban}\`**`
+          },
+          {
+            name: '**Possible solutions**',
+            value:
+              'Use this command inside a server where you have required permissions.'
+          },
+          {
+            name: '**Inputs given**',
+            value: `User: ${target}  \`${target.id}\``
+          },
+          {
+            name: '**Bot Error Dump**',
+            value: `${error}`
+          }
+        ]);
+
       await interaction.editReply({
-        components: [SupportRow],
-        content: `Unexpected Error Occured! \nPlease Report to the Developer. \nError Dump:\n\`${error}\``
+        components: [
+          SupportRow,
+          InviteRow
+        ],
+        embeds: [unban_fail]
       });
+      console.error(error);
     }
   }
 };
