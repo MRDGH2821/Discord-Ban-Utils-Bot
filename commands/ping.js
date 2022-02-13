1b1b97e88a2cd7e7e0359f26f00745f5700d4fa5
MRDGH2821 <mihirrabade@gmail.com>
2022-02-13 12:42:51 +0530

    feat: add more ping details

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -6,11 +7,25 @@ module.exports = {
     .setDescription('Replies with bot latency!'),
   async execute(interaction) {
     const sent = await interaction.reply({
-      content: 'Pinging...',
-      fetchReply: true
+        content: 'Pinging...',
+        fetchReply: true
+      }),
+      sentEmb = new MessageEmbed()
+        .setTitle('**Ping Results**')
+        .setColor('d8d4d3')
+        .addFields([
+          {
+            name: '**Round Trip Latency**',
+            value: `${sent.createdTimestamp - interaction.createdTimestamp} ms`
+          },
+          {
+            name: '**Websocket Heartbeat**',
+            value: `${interaction.client.ws.ping} ms`
+          }
+        ]);
+    await interaction.editReply({
+      content: 'Ping Results',
+      embeds: [sentEmb]
     });
-    await interaction.editReply(`Roundtrip latency: ${
-      sent.createdTimestamp - interaction.createdTimestamp
-    }ms`);
   }
 };
