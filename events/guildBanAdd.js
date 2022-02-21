899d2c9772a45b9ec452101cf691d5933679ba10
MRDGH2821 <mihirrabade@gmail.com>
2022-02-21 22:17:53 +0530

    fix: fix crashes when bot is banned or kicked

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -15,66 +15,68 @@ module.exports = {
   // eslint-disable-next-line sort-keys
   async execute(guildBan) {
     // console.log(guildBan);
-    const banLog = new MessageEmbed()
-        .setColor(EMBCOLORS.hammerHandle)
-        .setTitle('**Audit Ban Log**')
-        .setThumbnail(guildBan.user.displayAvatarURL({ dynamic: true }))
-        .setDescription(`\`${guildBan.user.tag}\` ${guildBan.user} got hit with the swift hammer of justice!\nID: \`${guildBan.user.id}\``)
-        .setTimestamp(),
-      fetchedLogs = await guildBan.guild.fetchAuditLogs({
-        limit: 1,
-        type: 'MEMBER_BAN_ADD'
-      }),
-      // since there's only 1 audit log entry in this collection, grab the first one
-      firstBanLog = fetchedLogs.entries.first(),
-      { executor, reason, target } = firstBanLog,
-      isBannedViaCmd = guildBan.client.user.id === executor.id;
-    if (!firstBanLog) {
-      banLog.addField([
-        {
-          name: '**Justice Ban Hammer Wielder**',
-          value:
-            'Unfortunately that could not be determined even from Audit logs'
-        },
-        {
-          name: '**Ban Reason**',
-          value: `${reason}`
-        }
-      ]);
-      console.log('No audit log found');
-    }
+    if (guildBan.user.id !== guildBan.client.user.id) {
+      const banLog = new MessageEmbed()
+          .setColor(EMBCOLORS.hammerHandle)
+          .setTitle('**Audit Ban Log**')
+          .setThumbnail(guildBan.user.displayAvatarURL({ dynamic: true }))
+          .setDescription(`\`${guildBan.user.tag}\` ${guildBan.user} got hit with the swift hammer of justice!\nID: \`${guildBan.user.id}\``)
+          .setTimestamp(),
+        fetchedLogs = await guildBan.guild.fetchAuditLogs({
+          limit: 1,
+          type: 'MEMBER_BAN_ADD'
+        }),
+        // since there's only 1 audit log entry in this collection, grab the first one
+        firstBanLog = fetchedLogs.entries.first(),
+        { executor, reason, target } = firstBanLog,
+        isBannedViaCmd = guildBan.client.user.id === executor.id;
+      if (!firstBanLog) {
+        banLog.addField([
+          {
+            name: '**Justice Ban Hammer Wielder**',
+            value:
+              'Unfortunately that could not be determined even from Audit logs'
+          },
+          {
+            name: '**Ban Reason**',
+            value: `${reason}`
+          }
+        ]);
+        console.log('No audit log found');
+      }
 
-    if (target.id === guildBan.user.id) {
-      banLog.addFields([
-        {
-          name: '**Justice Ban Hammer Wielder**',
-          value: `${executor} ${executor.tag}`
-        },
-        {
-          name: '**Ban Reason**',
-          value: `${reason}`
-        }
-      ]);
-    }
-    else {
-      banLog.addFields([
-        {
-          name: '**Justice Ban Hammer Wielder**',
-          value: 'Audit log fetch was inconclusive.'
-        },
-        {
-          name: '**Ban Reason**',
-          value: `${reason}`
-        }
-      ]);
-    }
-    if (!isBannedViaCmd) {
-      await sendHook(guildBan.client, banLog, guildBan.guild)
-        .then(() => console.log('Audit Ban Log sent!'))
-        .catch((error) => {
-          console.log('Audit Ban Log not sent due to error.\nError dump:');
-          console.error(error);
-        });
+      if (target.id === guildBan.user.id) {
+        banLog.addFields([
+          {
+            name: '**Justice Ban Hammer Wielder**',
+            value: `${executor} ${executor.tag}`
+          },
+          {
+            name: '**Ban Reason**',
+            value: `${reason}`
+          }
+        ]);
+      }
+      else {
+        banLog.addFields([
+          {
+            name: '**Justice Ban Hammer Wielder**',
+            value: 'Audit log fetch was inconclusive.'
+          },
+          {
+            name: '**Ban Reason**',
+            value: `${reason}`
+          }
+        ]);
+      }
+      if (!isBannedViaCmd) {
+        await sendHook(guildBan.client, banLog, guildBan.guild)
+          .then(() => console.log('Audit Ban Log sent!'))
+          .catch((error) => {
+            console.log('Audit Ban Log not sent due to error.\nError dump:');
+            console.error(error);
+          });
+      }
     }
   }
 };
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -15,23 +15,25 @@ module.exports = {
    */
   // eslint-disable-next-line sort-keys
   async execute(member) {
-    const exitLog = new MessageEmbed()
-      .setTimestamp()
-      .setTitle('**Audit Exit Log**')
-      .setColor(EMBCOLORS.wrenchHandle)
-      .setDescription(`${member.user.tag} ${member} left the server.\nID: \`${member.user.id}\``)
-      .addFields([
-        {
-          name: '**Joined at**',
-          value: time(member.joinedAt)
-        }
-      ]);
+    if (member.user.id !== member.client.user.id) {
+      const exitLog = new MessageEmbed()
+        .setTimestamp()
+        .setTitle('**Audit Exit Log**')
+        .setColor(EMBCOLORS.wrenchHandle)
+        .setDescription(`${member.user.tag} ${member} left the server.\nID: \`${member.user.id}\``)
+        .addFields([
+          {
+            name: '**Joined at**',
+            value: time(member.joinedAt)
+          }
+        ]);
 
-    await sendHook(member.client, exitLog, member.guild)
-      .then(() => console.log('Audit Exit Log sent!'))
-      .catch((error) => {
-        console.log('Audit Exit Log not sent due to error.\nError dump:');
-        console.error(error);
-      });
+      await sendHook(member.client, exitLog, member.guild)
+        .then(() => console.log('Audit Exit Log sent!'))
+        .catch((error) => {
+          console.log('Audit Exit Log not sent due to error.\nError dump:');
+          console.error(error);
+        });
+    }
   }
 };
