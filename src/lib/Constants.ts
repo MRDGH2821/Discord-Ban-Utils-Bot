52297c3a1227f11bb27faa12c96856ab9e3070e7
Mihir Rabade <mihirrabade@gmail.com>
2023-06-08 09:47:15 +0000

    feat: add color constants

---
+++ src/lib/Constants.ts
@@ -0,0 +1,10 @@
+export enum COLORS {
+  hammerHandle = 0xe1870a,
+  error = 0xff0033,
+  whiteGray = 0xd8d4d3,
+  wrenchHandle = 0x84929f,
+  invisible = 0x2f3036,
+  freeze = 0x65919d,
+}
+
+export const EMPTY_STRING = "\u200b";
