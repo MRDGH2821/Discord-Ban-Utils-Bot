e8383864b3ba15730ac282839b3600b9f18970a4
Mihir Rabade <mihirrabade@gmail.com>
2023-06-25 17:11:10 +0000

    chore: update typeDefs.ts

--- src/lib/typeDefs.ts
+++ src/lib/typeDefs.ts
@@ -1,3 +1,15 @@
+import type {
+  Guild, Message, TextBasedChannel, User,
+} from 'discord.js';
+
 export type BanEntity = string;
 export type BanEntityWithReason = { id: string; reason: string | null | undefined };
 export type BanType = BanEntity | BanEntityWithReason;
+
+export type BanExportOptions = {
+  sourceGuild: Guild;
+  requesterUser: User;
+  notifyInChannel: TextBasedChannel;
+  includeReason: boolean;
+  sourceMessage: Message;
+};
