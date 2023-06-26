60f036fd64570f900623712e91b84bd54e6b6584
MRDGH2821 <mihirrabade@gmail.com>
2023-06-26 14:49:49 +0530

    feat: create EnvConfig.ts

---
+++ src/lib/EnvConfig.ts
@@ -0,0 +1,9 @@
+if (process.env.DISCORD_TOKEN === undefined) {
+  throw new Error('Token not defined');
+}
+
+const EnvConfig = {
+  token: process.env.DISCORD_TOKEN,
+};
+
+export default EnvConfig;
