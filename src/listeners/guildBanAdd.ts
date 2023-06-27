7ea5950df9bec358eda16cd9f8d9ffde658ea239
Mihir Rabade <mihirrabade@gmail.com>
2023-06-27 09:07:04 +0000

    feat: create guildBanAdd.ts

---
+++ src/listeners/guildBanAdd.ts
@@ -0,0 +1,51 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Events, Listener } from '@sapphire/framework';
+import { AuditLogEvent, GuildBan, type APIEmbed } from 'discord.js';
+import { COLORS } from '../lib/Constants';
+import db from '../lib/Firestore';
+
+const config = db.collection('config');
+
+@ApplyOptions<Listener.Options>({
+  name: 'New Guild Ban',
+  event: Events.GuildBanAdd,
+})
+export default class UserEvent extends Listener<typeof Events.GuildBanAdd> {
+  public override async run(ban: GuildBan) {
+    const banLogs = await ban.guild.fetchAuditLogs({
+      type: AuditLogEvent.MemberBanAdd,
+      limit: 1,
+    });
+
+    const firstBanLog = banLogs.entries.first();
+
+    const executor = firstBanLog?.executor;
+    const reason = ban?.reason || firstBanLog?.reason || 'No reason provided';
+
+    const isBannedViaCmd = executor?.id === this.container.client.user?.id;
+    if (isBannedViaCmd) return;
+
+    const banEmbed: APIEmbed = {
+      title: '**Audit Ban Log**',
+      color: COLORS.hammerHandle,
+      description: `\`${ban.user.username}\` ${ban.user} got hit with the swift hammer of justice!\nID: \`${ban.user.id}\`\n\nReason: ${reason}`,
+      timestamp: new Date().toISOString(),
+    };
+
+    if (firstBanLog) {
+      banEmbed.fields?.push({
+        name: '**Justice Ban Hammer Wielder**',
+        value: `${executor?.username} ${executor}`,
+      });
+    } else {
+      banEmbed.fields?.push({
+        name: '**Justice Ban Hammer Wielder**',
+        value: 'Cannot be determined (even from Audit Log)',
+      });
+    }
+
+    const data = (await config.doc(ban.guild.id).get()).data();
+
+    // TODO: Send webhook message log
+  }
+}
