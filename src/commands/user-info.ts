85dec3fc96bf84231f348fa6922722488938dd30
Mihir Rabade <mihirrabade@gmail.com>
2023-07-03 06:57:22 +0000

    feat: create user-info.ts

---
+++ src/commands/user-info.ts
@@ -0,0 +1,53 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Command } from '@sapphire/framework';
+import { ApplicationCommandOptionType, time, type APIEmbed } from 'discord.js';
+import { COLORS } from '../lib/Constants';
+
+@ApplyOptions<Command.Options>({
+  name: 'user-info',
+  description: 'A basic slash command',
+})
+export default class UserCommand extends Command {
+  public override registerApplicationCommands(registry: Command.Registry) {
+    registry.registerChatInputCommand({
+      name: this.name,
+      description: this.description,
+      options: [
+        {
+          name: 'user',
+          description: 'Put user ID or SElect a user to see their info',
+          type: ApplicationCommandOptionType.User,
+          required: true,
+        },
+      ],
+    });
+  }
+
+  public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+    const user = interaction.options.getUser('user', true);
+    const embed: APIEmbed = {
+      title: '**User info**',
+      color: COLORS.whiteGray,
+      thumbnail: {
+        url: user.displayAvatarURL(),
+      },
+      fields: [
+        {
+          name: 'User ID',
+          value: user.id,
+        },
+        {
+          name: 'Account created on',
+          value: `${time(user.createdAt)} (${time(user.createdAt, 'R')})`,
+        },
+      ],
+      timestamp: new Date().toISOString(),
+    };
+    if (interaction.channel?.isDMBased()) {
+      embed.fields?.push({
+        name: '**Can this user invite the bot?**',
+        value: `${!user.bot}`,
+      });
+    }
+  }
+}
