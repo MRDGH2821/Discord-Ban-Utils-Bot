baa27a3d5f1226c6f50be973792bacf30a074bba
MRDGH2821 <mihirrabade@gmail.com>
2023-07-12 20:22:24 +0530

    feat: show additional user info
    feat(user-info.ts): add support for displaying user's display name and joined date in the user info command
    feat(user-info.ts): add support for displaying bot's powers and user's powers if the user is a moderator
    feat(user-info.ts): add support for displaying top 5 roles of the user if the user is a moderator

--- src/commands/user-info.ts
+++ src/commands/user-info.ts
@@ -8,7 +8,7 @@ import {
   time,
   type APIEmbed,
 } from 'discord.js';
-import { COLORS } from '../lib/Constants';
+import { COLORS, EMPTY_STRING } from '../lib/Constants';
 import { jumpLink } from '../lib/utils';
 
 @ApplyOptions<Command.Options>({
@@ -44,55 +44,54 @@ export default class UserCommand extends Command {
         {
           name: 'Username',
           value: user.bot ? user.tag : user.username,
+          inline: true,
         },
         {
           name: 'User ID',
           value: user.id,
+          inline: true,
         },
         {
           name: 'Account created on',
           value: `${time(user.createdAt)} (${time(user.createdAt, 'R')})`,
+          inline: true,
         },
       ],
       timestamp: new Date().toISOString(),
+    };
+
+    const inviteButton = {
+      type: ComponentType.Button,
+      label: 'Invite the bot in your Server!',
+      style: 5,
+      url: this.container.client.generateInvite({
+        scopes: [OAuth2Scopes.Bot, OAuth2Scopes.ApplicationsCommands, OAuth2Scopes.Guilds],
+        permissions: [
+          PermissionFlagsBits.AttachFiles,
+          PermissionFlagsBits.BanMembers,
+          PermissionFlagsBits.EmbedLinks,
+          PermissionFlagsBits.KickMembers,
+          PermissionFlagsBits.ManageWebhooks,
+          PermissionFlagsBits.ModerateMembers,
+          PermissionFlagsBits.SendMessages,
+          PermissionFlagsBits.SendMessagesInThreads,
+          PermissionFlagsBits.UseExternalEmojis,
+          PermissionFlagsBits.ViewAuditLog,
+          PermissionFlagsBits.ViewChannel,
+        ],
+      }),
+    };
+
+    const inviteButtonRow = {
+      type: ComponentType.ActionRow,
+      components: [inviteButton],
     };
 
     if (!interaction.inGuild() || !interaction.guild) {
       const iRes = await interaction.reply({ embeds: [embed] });
       if (!user.bot) {
         return iRes.edit({
-          components: [
-            {
-              type: ComponentType.ActionRow,
-              components: [
-                {
-                  type: ComponentType.Button,
-                  label: 'Invite the bot in your Server!',
-                  style: 5,
-                  url: this.container.client.generateInvite({
-                    scopes: [
-                      OAuth2Scopes.Bot,
-                      OAuth2Scopes.ApplicationsCommands,
-                      OAuth2Scopes.Guilds,
-                    ],
-                    permissions: [
-                      PermissionFlagsBits.AttachFiles,
-                      PermissionFlagsBits.BanMembers,
-                      PermissionFlagsBits.EmbedLinks,
-                      PermissionFlagsBits.KickMembers,
-                      PermissionFlagsBits.ManageWebhooks,
-                      PermissionFlagsBits.ModerateMembers,
-                      PermissionFlagsBits.SendMessages,
-                      PermissionFlagsBits.SendMessagesInThreads,
-                      PermissionFlagsBits.UseExternalEmojis,
-                      PermissionFlagsBits.ViewAuditLog,
-                      PermissionFlagsBits.ViewChannel,
-                    ],
-                  }),
-                },
-              ],
-            },
-          ],
+          components: [inviteButtonRow],
         });
       }
       return iRes;
@@ -101,25 +100,67 @@ export default class UserCommand extends Command {
     const member = await interaction.guild.members.fetch(user);
     const mod = await interaction.guild.members.fetch(interaction.member.user.id);
 
-    const botModStats = `Can Ban: \`${member.bannable}\`\nCan Kick: \`${member.kickable}\`\nCan Moderate (timeout, etc.): \`${member.moderatable}\`\nIs above: \`${member.manageable}\``;
-
     embed.fields?.push({
-      name: '**This bot ...**',
-      value: botModStats,
+      name: '**Display name**',
+      value: member.displayName,
+      inline: true,
     });
 
-    const userModStats = `Can Ban: \`${mod.permissions.has(
-      PermissionFlagsBits.BanMembers,
-    )}\`\nCan Kick: \`${mod.permissions.has(
-      PermissionFlagsBits.KickMembers,
-    )}\`\nCan Moderate (timeout, etc.): \`${mod.permissions.has(
-      PermissionFlagsBits.ModerateMembers,
-    )}\`\nAre above: \`${mod.roles.highest.comparePositionTo(member.roles.highest) > 0}\``;
+    const { joinedAt } = member;
+    if (joinedAt) {
+      embed.fields?.push({
+        name: '**Joined on**',
+        value: `${time(joinedAt)} (${time(joinedAt, 'R')})`,
+        inline: true,
+      });
+    }
 
-    embed.fields?.push({
-      name: '**You ...**',
-      value: userModStats,
+    const isActuallyMod = mod.permissions.has(PermissionFlagsBits.ModerateMembers);
+
+    if (isActuallyMod) {
+      embed.fields?.push({
+        name: EMPTY_STRING,
+        value: EMPTY_STRING,
+      });
+
+      const botModStats = `Can Ban: \`${member.bannable}\`\nCan Kick: \`${member.kickable}\`\nCan Moderate (timeout, etc.): \`${member.moderatable}\`\nIs above ${member}: \`${member.manageable}\``;
+
+      embed.fields?.push({
+        name: `**Powers of ${this.container.client.user?.tag}**`,
+        value: botModStats,
+        inline: true,
+      });
+
+      const userModStats = `Can Ban: \`${mod.permissions.has(
+        PermissionFlagsBits.BanMembers,
+      )}\`\nCan Kick: \`${mod.permissions.has(
+        PermissionFlagsBits.KickMembers,
+      )}\`\nCan Moderate (timeout, etc.): \`${mod.permissions.has(
+        PermissionFlagsBits.ModerateMembers,
+      )}\`\nIs above ${member}: \`${
+        mod.roles.highest.comparePositionTo(member.roles.highest) > 0
+      }\``;
+
+      embed.fields?.push({
+        name: '**Your Powers**',
+        value: userModStats,
+        inline: true,
+      });
+
+      embed.fields?.push({
+        name: '**Top 5 Roles**',
+        value: member.roles.cache
+          .sort((a, b) => b.position - a.position)
+          .filter((r) => !r.managed)
+          .map((r) => r.toString())
+          .filter((r) => r !== '@everyone')
+          .splice(0, 5)
+          .join(' '),
+      });
+    }
+    return interaction.reply({
+      embeds: [embed],
+      components: isActuallyMod ? undefined : [inviteButtonRow],
     });
-    return interaction.reply({ embeds: [embed] });
   }
 }
