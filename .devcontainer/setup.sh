a465d2a9ea92f18d7261032b855d736eb9869bcf
MRDGH2821 <ask.mrdgh2821@outlook.com>
2023-07-22 18:10:42 +0530

    chore: modify commands

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Installing Firefox ESR";
-sudo apt-get update;
-export DEBIAN_FRONTEND=noninteractive;
-sudo apt-get install -y firefox-esr;
-
-echo "Installing Project Dependencies";
-sudo npm install;
+echo "Installing Firefox ESR"
+sudo apt-get update
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get install -y firefox-esr
