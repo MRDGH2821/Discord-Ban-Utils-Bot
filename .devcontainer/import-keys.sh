5d9f23aba4bbd47dad76fd75050de990a8d3a17d
MRDGH2821 <ask.mrdgh2821@outlook.com>
2023-07-28 08:20:19 +0000

    refactor: change import script
    - Skip key import if running in GitHub Codespaces
    - Check for required key files and import accordingly

--- .devcontainer/import-keys.sh
+++ .devcontainer/import-keys.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/bash
 
-# Check if .devcontainer/gpg folder exists
-if [ -d .devcontainer/gpg ]; then
-  echo "Importing Public Keys"
-  sudo gpg --import .devcontainer/gpg/public-keys.asc
-
-  echo "Importing Private Keys"
-  sudo gpg -a --passphrase-file=.devcontainer/gpg/gpg-key-password.txt --allow-secret-key-import --import .devcontainer/gpg/private-keys.asc
-
-  echo "Importing Trust DB"
-  sudo gpg --import-ownertrust .devcontainer/gpg/owner-trust-db.txt
+# Check if the script is running on GitHub Codespaces
+if [[ ! -z "${CODESPACES}" ]]; then
+  echo "Running in GitHub Codespaces.\nNo need to import any keys."
 else
-  echo "The .devcontainer/gpg folder does not exist. Skipping import."
+  # Check if .devcontainer/gpg folder has required files
+  if [ -f ".devcontainer/gpg/public-keys.asc" ]; then
+    echo "Importing Public Keys"
+    sudo gpg --import .devcontainer/gpg/public-keys.asc
+  else
+    echo "public-keys.asc file not found"
+  fi
+
+  if [ -f ".devcontainer/gpg/private-keys.asc" ]; then
+    echo "Importing Private Keys"
+    sudo gpg -a --passphrase-file=.devcontainer/gpg/gpg-key-password.txt --allow-secret-key-import --import .devcontainer/gpg/private-keys.asc
+  else
+    echo "private-keys.asc file not found"
+  fi
+
+  if [ -f ".devcontainer/gpg/owner-trust-db.txt" ]; then
+    echo "Importing Trust DB"
+    sudo gpg --import-ownertrust .devcontainer/gpg/owner-trust-db.txt
+  else
+    echo "owner-trust-db.txt file not found"
+  fi
+
 fi
