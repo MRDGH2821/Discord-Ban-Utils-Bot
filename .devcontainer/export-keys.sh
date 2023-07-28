990f22f2c710108bbb4e92dc588aafd3fee36bd9
MRDGH2821 <ask.mrdgh2821@outlook.com>
2023-07-28 07:50:42 +0000

    chore: modify docs & scripts to import specific key

--- .devcontainer/export-keys.sh
+++ .devcontainer/export-keys.sh
@@ -9,5 +9,15 @@ else
   echo "Running on Linux or macOS host"
   # Add your GPG export and import commands here for Linux and macOS
 
-  bash .devcontainer/export-keys.cmd
+  fileContent=$(cat .devcontainer/gpg/gpg-key-id.txt)
+
+  echo "Exporting Public Keys"
+  gpg -a --export --output .devcontainer/gpg/public-keys.asc --yes
+
+  echo "Exporting Private Key"
+  gpg -a --passphrase-file=.devcontainer/gpg/gpg-key-password.txt --export-secret-keys $fileContent --output .devcontainer/gpg/private-keys.asc --yes
+
+  echo "Exporting Trust DB"
+  gpg -a --export-ownertrust --output .devcontainer/gpg/owner-trust-db.txt >.devcontainer/gpg/owner-trust-db.txt
+
 fi
--- .devcontainer/import-keys.sh
+++ .devcontainer/import-keys.sh
@@ -2,13 +2,13 @@
 
 # Check if .devcontainer/gpg folder exists
 if [ -d .devcontainer/gpg ]; then
-  #echo "Importing Public Keys"
-  #sudo gpg --import .devcontainer/gpg/public-keys.asc
+  echo "Importing Public Keys"
+  sudo gpg --import .devcontainer/gpg/public-keys.asc
 
-  echo "\nImporting Private Keys"
-  sudo gpg -a --passphrase-file=.devcontainer/gpg/gpg-password.txt --import .devcontainer/gpg/private-keys.asc
+  echo "Importing Private Keys"
+  sudo gpg -a --passphrase-file=.devcontainer/gpg/gpg-key-password.txt --allow-secret-key-import --import .devcontainer/gpg/private-keys.asc
 
-  echo "\nImporting Trust DB"
+  echo "Importing Trust DB"
   sudo gpg --import-ownertrust .devcontainer/gpg/owner-trust-db.txt
 else
   echo "The .devcontainer/gpg folder does not exist. Skipping import."
