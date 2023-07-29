9512d83eb42fce74757bea1e61d899c00f1fe994
MRDGH2821 <ask.mrdgh2821@outlook.com>
2023-07-29 13:07:53 +0000

    chore: update command for automated experience

--- .devcontainer/export-keys.cmd
+++ .devcontainer/export-keys.cmd
@@ -10,7 +10,7 @@ echo Exporting Public Keys
 gpg -a --output .devcontainer/gpg/public-keys.asc --yes --export %fileContent%
 
 echo Exporting Private Key
-gpg -a --pinentry-mode loopback --passphrase-file=.devcontainer/gpg/gpg-key-password.txt --output .devcontainer/gpg/private-keys.asc --export-secret-keys %fileContent%
+gpg -a --pinentry-mode loopback --passphrase-file=.devcontainer/gpg/gpg-key-password.txt --output .devcontainer/gpg/private-keys.asc --yes --export-secret-keys %fileContent%
 
 echo Exporting Trust DB
 gpg -a --export-ownertrust >.devcontainer/gpg/owner-trust-db.txt
