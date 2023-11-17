1a7ddf2f40305b47820688f631d0cbd2dec7a7af
MRDGH2821 <ask.mrdgh2821@outlook.com>
2023-11-17 13:51:51 +0000

    feat: print bot name after login

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ const main = async () => {
   try {
     client.logger.info('Logging in');
     await client.login();
-    client.logger.info('Logged in');
+    client.logger.info(`Logged in as: ${client.user?.tag}`);
   } catch (error) {
     client.logger.fatal(error);
     client.destroy();
