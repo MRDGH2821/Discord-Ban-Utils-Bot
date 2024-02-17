840db017c9318fd659f7b6c6a9ee6099d653a3de
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-02-17 15:49:17 +0000

    feat: add complete logic for exclusion list listener

--- src/listeners/ExclusionListUpdate.ts
+++ src/listeners/ExclusionListUpdate.ts
@@ -1,5 +1,6 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Listener } from '@sapphire/framework';
+import db from '../lib/Database';
 import type { ExclusionListUpdateOptions } from '../lib/EventTypes';
 import { BUEvents } from '../lib/EventTypes';
 
@@ -10,8 +11,19 @@ const PIECE_NAME = 'Update Exclusion List';
 })
 export default class UserEvent extends Listener {
   public override run(payload: ExclusionListUpdateOptions) {
-    this.container.logger.debug('Exclusion list updated:', payload);
+    this.container.logger.debug('Updating Exclusion list:', payload);
+    const { mode } = payload;
 
-    // todo: add logic to update the exclusion list
+    db.exclusionList
+      .upset(payload.guildId, ($) => {
+        const func = mode === 'add' ? $.arrayUnion : $.arrayRemove;
+        return {
+          guildId: payload.guildId,
+          exportExclusion: func(payload.exportExclusion),
+          importExclusion: func(payload.importExclusion),
+        };
+      })
+      .then(this.container.logger.debug)
+      .catch(this.container.logger.error);
   }
 }
