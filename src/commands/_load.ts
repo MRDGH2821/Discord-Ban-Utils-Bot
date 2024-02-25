4cdd118240a0a8d95c38c36e556f60a9843ae2a8
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-02-25 18:34:33 +0000

    chore: update auto built artifacts

--- src/commands/_load.ts
+++ src/commands/_load.ts
@@ -6,6 +6,7 @@ import './test.js';
 import './export-ban-list.js';
 import './user-info.js';
 import './timeout.js';
+import './about.js';
 import './mass.js';
 import './ban.js';
 import './import-ban-list.js';
